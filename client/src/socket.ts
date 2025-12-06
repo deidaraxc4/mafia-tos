c698643d3435261fdab38a9cc9dda1215c25dbb4
Nick Tang <tangnick@live.com>
2025-12-05 21:05:43 -0500

    more debug

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -2,6 +2,8 @@ import { io, Socket } from 'socket.io-client';
 
 const SOCKET_SERVER_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001'; 
 
+console.log(`socket url is ${SOCKET_SERVER_URL}`)
+
 export const socket: Socket = io(SOCKET_SERVER_URL, {
     // Optional configuration
     reconnectionAttempts: 5,
